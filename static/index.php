0fe4798f4fe2507d2a12747bc6400c7cdd56539c
Travis <travis@travis-ci.org>
2018-08-22 15:32:38 +1000

    added index php file

---
+++ static/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+	<!-- Styling for public area -->
+	<link rel="stylesheet" href="static/css/public_styling.css">
+	<meta charset="UTF-8">
+	<title>Piano and Music Society | Home </title>
+</head>
+<body>
+	<!-- container - wraps whole page -->
+	<div class="container">
+		<!-- horizontal navbar -->
+		<div class="navbar">
+			<div class="logo_div">
+				<a href="index.php"><h1>LifeBlog</h1></a>
+			</div>
+			  <a class="active" href="index.php">Home</a>
+			  <a href="#news">News</a>
+			  <a href="#contact">Contact</a>
+			  <a href="#about">About</a>
+		</div>
+		<!-- // horizontal navbar -->
+
+		<!-- Page content -->
+		<div class="content">
+			<h2 class="content-title">Recent Articles</h2>
+			<hr>
+			<!-- more content still to come here ... -->
+		</div>
+		<!-- // Page content -->
+
+		<!-- footer -->
+		<div class="footer">
+			<p>MyViewers &copy; <?php echo date('Y'); ?></p>
+		</div>
+		<!-- // footer -->
+
+	</div>
+	<!-- // container -->
+</body>
+</html>
